ab992567664240ada4a85dd28262aa9b025405ca
Aasiya Naaz <aasiyanaaz2003@gmail.com>
2025-11-15 00:36:47 +0530

    minor adjustments

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import Navigation from "@/components/Navigation";
 import HomePage from "@/components/HomePage";
+import { useAppContext } from "@/hooks/useAppContext";
 
 export default function Home() {
+  const { darkMode, setDarkMode } = useAppContext();
+
   return (
     <>
-      <Navigation />
-      <HomePage />
+      <Navigation darkMode={darkMode} setDarkMode={setDarkMode} />
+      <HomePage darkMode={darkMode} setDarkMode={setDarkMode} />
     </>
   );
 }
